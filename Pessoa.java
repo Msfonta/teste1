1e12e98e6666e9cc6588aaf32a1463ada8f66e37
Marco Senna Fontanari <marcosennafontanari@gmail.com>
2017-02-18 21:31:53 -0200

    adicionando conteudo ao arquivo

--- Pessoa.java
+++ Pessoa.java
@@ -1,11 +1,11 @@
 public class Pessoa { 
 	String nome;
 	double peso;
-	String profissao;
 	
-	public Pessoa(String nome, double peso, String profissao){
+	
+	public Pessoa(String nome, double peso){
 		this.nome = nome;
 		this.peso = peso;
-		this.profissao = profissao;
+		
 	}
 }
