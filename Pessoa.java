c13d28d9a103ef96a3403d2b45c6f3eae3c738da
Marco Senna Fontanari <marcosennafontanari@gmail.com>
2017-02-18 20:25:40 -0200

    Alterando a classe Pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -1,9 +1,11 @@
 public class Pessoa { 
 	String nome;
 	int idade;
+	String profissao;
 	
-	public Pessoa(String nome, int idade){
+	public Pessoa(String nome, int idade, String profissao){
 		this.nome = nome;
 		this.idade = idade;
+		this.profissao = profissao;
 	}
 }
